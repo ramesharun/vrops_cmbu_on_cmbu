9d6c0b6f6c907ac0b43ea739583bca1c4b409cef
ramesharun <ramesh95arun@yahoo.com>
2021-03-08 13:19:36 -0800

    Update variables.tf

--- vrops/provisioning/variables.tf
+++ vrops/provisioning/variables.tf
@@ -1,62 +1,195 @@
-# Security Group Outputs 
-
-output "sg_id" {
-  description = "sg_id"
-  value   = aws_security_group.vrops-sg.id
- }
-
-output "sg_arn" {
-  description = "sg_arn"
-  value   = aws_security_group.vrops-sg.arn
- }
-
-output "sg_name" {
-  description = "sg_name"
-  value   = aws_security_group.vrops-sg.name
- }
-
-output "sg_vpc_id" {
-  description = "sg_vpc_id"
-  value   = aws_security_group.vrops-sg.vpc_id
+variable "env" {                      # POD FQDN Group Name
+  type = string
+  default="dev"
 }
 
-output "sg_owner_id" {
-  description = "sg_owner_id"
-  value   = aws_security_group.vrops-sg.owner_id
+variable "ami_id" {                   # AMI ID
+  type = string
+  default="ami-0a36eb8fadc976275"
 }
 
-output "sg_description" {
-  description = "sg_description"
-  value   = aws_security_group.vrops-sg.description
+variable "cluster_size" {              # Flavor T-shirt compute size
+  type = string
+  default="extrasmall"
 }
 
-output "sg_revoke_rules_on_delete" {
-  description = "sg_revoke_rules_on_delete"
-  value   = aws_security_group.vrops-sg.revoke_rules_on_delete
+variable "pod_fqdn_name" {              # POD FQDN Group Name
+  type = string
 }
 
- output "aws_instances_id" {
-   description = "AWS instance ids provisioned"
-   value   = tolist(aws_instance.vrops-node.*.id)
- }
+variable "ha_enabled" {            # HA Enabled Flag
+  default = false                  # 
+}
 
-output "aws_az_subnet_id" {
-   value = element(tolist(data.aws_subnet_ids.az_subnets.ids),0)
- }
+variable "ca_enabled" {            # CA Enabled Flag
+  default = false                # 
+}
 
- output "aws_all_subnet_id" {
-   value = tolist(data.aws_subnet_ids.all_subnets.ids)
- }
+variable "pendo_enabled" {            # Pendo Enabled Flag
+  default = false                # 
+}
 
- output "elb_dns_name" {
-   value = aws_elb.vrops_elb.dns_name
- }
+variable "node_count" {            # Node Count
+  type    = number
+  default = 1                   # 
+}
 
- output "elb_zone_id" {
-   value = aws_elb.vrops_elb.zone_id
- }
+variable "sc_environment" {            # SC_Environment
+  type    = string
+  default = "sc_env"
 
- output "vrops_zone_id"{
+}
 
-   value = data.aws_route53_zone.vrops.id
- }
+variable "pool_instance_id" {            # Pool Instance Id
+  type    = string
+  default = "sc_env"
+
+}
+
+variable "resource_id" {            # Resource Id 
+  type    = string
+  default = "sc_env"
+
+}
+
+variable "admin_password" {            # Admin Password
+  type    = string
+  default = "sc_env"
+
+}
+
+variable "availability_zones" {            # Availability zone
+  type    = string
+  default = "us-west-2a"
+
+}
+
+variable "witness_availability_zone" {            # Witness Availability zone 
+  type    = string
+  default = "sc_env"
+}
+
+variable "vpc_id" {
+  type = map 
+  default = {
+    "dev" = "vpc-02608d56dec071559"
+    "stage" = ""
+    "prod" = ""
+  }
+}
+
+variable "instance_size" {
+  type = map 
+  default = {
+    "extrasmall" = "m5.large"
+    "small" = "m5.xlarge"
+    "medium" = "m5.2xlarge"
+    "large" = "m5.4xlarge"
+    "extralarge" = "m5.8xlarge"
+  }
+}
+
+variable "disk_size" {
+  type = map 
+  default = {
+    "extrasmall" = 250
+    "small" = 300
+    "medium" = 600
+    "large" = 1200
+    "extralarge" = 1200
+  }
+}
+
+variable "tcp_ports" {
+  description = "tcp ports for security group"
+  type        = list(number)
+  default     = [ 22, 80, 5432, 8080, 7001, 6061, 8888, 10000, 9042, 5433 ,443, 12016]
+}
+
+variable "udp_ports" {
+  description = "udp ports for security group"
+  type        = list(number)
+  default     = [8888]
+}
+
+variable "gw_tcp_ports" {
+  description = "gw_tcp_ports for security group"
+  type        = list(number)
+  default     = [80, 8080, 443]
+}
+
+variable "sc_tcp_ports" {
+  description = "sc_tcp_ports for security group"
+  type        = list(number)
+  default     = [80, 8080, 443]
+}
+
+variable "oc_tcp_ports" {
+  description = "oc_tcp_ports for security group"
+  type        = list(number)
+  default     = ["22","80", "8080", "443"]
+}
+
+variable "jenkins_ssm_ports" {
+  description = "jenkins_ssm_ports for security group"
+  type        = list(number)
+  default     = [22, 80, 443]
+}
+
+variable "xcenter_ports" {
+  description = "xcenter_ports for security group"
+  type        = list(number)
+  default     = [22, 80, 443]
+}
+
+variable "ovpn_ports" {
+  description = "ovpn_ports for security group"
+  type        = list(number)
+  default     = [22, 80, 443]
+}
+
+variable "vrops_sre_org_ports" {
+  description = "vrops_sre_org_ports for security group"
+  type        = list(number)
+  default     = [80, 443]
+}
+
+variable "range_from_tcp_ports" {
+  description = "tcp ports for security group"
+  type        = list(number)
+  default     = [10002,20002]
+}
+
+variable "range_to_tcp_ports" {
+  description = "tcp ports for security group"
+  type        = list(number)
+  default     = [10010,20010]
+}
+
+variable "ssh_key_name" {
+  description = "ssh key pair"
+  type = string
+  default = "vropskey"
+}
+
+
+variable "ssh_arn" {
+  description = "ssh arn"
+  type = string
+  default = "arn:aws:acm:us-west-2:000954396075:certificate/7a6ae625-841e-4a6c-b3b1-8fde058e8439"
+}
+
+variable "dns_record_type" {
+  description = "dns record type"
+  type = string
+  default = "A"
+}
+
+variable "dns_ttl" {
+  type = string
+  default = "30"
+}
+
+variable "hosted_zone" {
+  default = "vrops-ops.com"
+}
